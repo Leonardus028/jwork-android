5f2da52872998c82f6b7f35097e52ae354c7b67b
Leonardus Kevin <leonardus.kevin@ui.ac.id>
2021-05-27 18:06:26 +0700

    Modul 9 - Case Study

--- app/src/main/java/com/example/jwork_android/LoginActivity.java
+++ app/src/main/java/com/example/jwork_android/LoginActivity.java
@@ -42,6 +42,8 @@ public class LoginActivity extends AppCompatActivity {
                             JSONObject jsonObject = new JSONObject(response);
                             if (jsonObject != null) {
                                 Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
                             }
                         } catch (JSONException e) {
                             Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
@@ -63,5 +65,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
